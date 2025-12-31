2b69b34e936a3f97f0d9c1a0144020ec16075f6e
Ariful Islam <arifulq234@gmail.com>
2026-01-01 00:13:18 +0600

    Padding aded on dashboard routes

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const layout = ({children}) => {
     return (
-        <div className='grid grid-cols-12 gap-1 mx-auto'>
+        <div className='grid grid-cols-12 gap-1 mx-auto px-4 mt-1'>
             <div className='col-span-3 flex flex-col gap-2 rounded-2xl justify-center'>
-                <Link className='bg-gray-500' href='/dashboard/add-story'>Add Storie</Link>
-                <Link className='bg-gray-500' href='/dashboard/manage-story'>Manage Story</Link>
+                <Link className='bg-gray-500 px-3 py-1' href='/dashboard/add-story'>Add Storie</Link>
+                <Link className='bg-gray-500 px-3 py-1' href='/dashboard/manage-story'>Manage Story</Link>
             </div>
             <div className='col-span-8'>
                 {children}
